2581cf61e09a520129c2cf32c91e475364918ae5
j-gens <jgens2@gmail.com>
2020-02-03 11:07:07 -0800

    Import style sheet into monster-list component

--- client/components/monster-list/monster-list.component.jsx
+++ client/components/monster-list/monster-list.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import MonsterListEntry from '../monster/monster.component.jsx';
+import './monster-list.styles.css';
 
 const MonsterList = (props) => (
-  <div>
+  <div className="monster-list">
     {props.monsterList.map(monster =>
       <MonsterListEntry monster={monster} key={monster.id} />
     )}
