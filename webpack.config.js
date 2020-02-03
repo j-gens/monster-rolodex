64146e7f4ca918ef4b57c4333da038fcac6000a4
j-gens <jgens2@gmail.com>
2020-02-03 09:56:49 -0800

    Update webpack config with @babel presets

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: "development",
   entry: "./client/index.js",
   output: {
     filename: "bundled.js",
@@ -9,7 +10,12 @@ module.exports = {
       test: /\.js/,
       exclude: /node_modules/,
       use: {
-        loader: "babel-loader"
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-react", "@babel/preset-env"
+          ]
+        }
       }
     }]
   },
