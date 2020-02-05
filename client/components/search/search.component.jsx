cded59d0f117f18d869d6e4caca725dd4a680ec5
j-gens <jgens2@gmail.com>
2020-02-05 12:00:58 -0800

    Add boilerplate search component and style sheet

---
+++ client/components/search/search.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search.styles.css';
+
+const Search = () => {
+  return (
+    <div className="search-bin">
+      search bar goes here
+    </div>
+  )
+}
+
+export default Search;
