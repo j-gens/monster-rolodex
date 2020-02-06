4fb5dc079b33ea496d13d9e0b9126c3c492e7a79
j-gens <jgens2@gmail.com>
2020-02-05 16:34:44 -0800

    Implement filter function on monster list

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -20,21 +20,21 @@ class App extends React.Component {
   }
 
   handleSearch(e) {
+    console.log(e.target.value)
     this.setState({ searchField: e.target.value })
   }
 
   render() {
     const { monsterList, searchField } = this.state;
-    const filteredMonsterList = monsterList.filter(monster => {
-      console.log(monster.name.toLowerCase());
-      monster.name.toLowerCase().includes(searchField.toLowerCase());
-    })
+    const filteredMonsterList = monsterList.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
 
     return (
       <>
         <div>Monsters Rolodex</div>
         <Search placeholder={this.state.placeholder} handleSearch={this.handleSearch} />
-        <MonsterList monsterList={this.state.monsterList} />
+        <MonsterList monsterList={filteredMonsterList} />
       </>
     )
   }
